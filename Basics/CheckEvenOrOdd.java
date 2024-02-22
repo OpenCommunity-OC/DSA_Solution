fe618feb7a3f8639c7ff17138b082c65c3296593
vicky acharjee <vickyacharjeevlg@gmail.com>
2024-02-22 13:52:12 +0530

    added for CheckEvenOdd

---
+++ Basics/CheckEvenOrOdd.java
@@ -0,0 +1,13 @@
+public class CheckEvenOrOdd {
+    public static void main(String[] args) {
+        int num=122;
+
+        if(num%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+    
+}
