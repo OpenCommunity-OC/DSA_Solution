1ce4a156241972538fa672aa63435d3c1afd9d56
vicky acharjee <vickyacharjeevlg@gmail.com>
2024-02-23 22:05:31 +0530

    added for NestedMultiplication

---
+++ Basics/nestedMultiplication.java
@@ -0,0 +1,15 @@
+package Basics;
+
+public class nestedMultiplication {
+    public static void main(String[] args) {
+        
+        //Mandatory Range from 1 to 10
+        for (int i = 1; i <= 20; i++) {
+            for (int j = 0; j <=10; j++) {
+                System.out.println(i+"X"+j+"="+i*j);
+            }
+            
+        }
+    }
+ 
+}
