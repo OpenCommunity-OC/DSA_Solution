518dd3ee7e8218fa8242f1d9075513c21023ba42
vicky acharjee <vickyacharjeevlg@gmail.com>
2024-03-05 17:51:31 +0530

    added for Basics of ArrayListD

---
+++ Array_ArrayList/ArrayLists.java
@@ -0,0 +1,24 @@
+package Array_ArrayList;
+import java.util.ArrayList;
+
+public class ArrayLists {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(10);
+        
+        // Now you can use the ArrayList<Integer> 'list' as needed.
+        // For example, add elements to the list:
+        list.add(5);
+        list.add(10);
+        list.add(10);
+        list.add(511);
+        list.add(102);
+        list.add(103);
+        list.add(54);
+        list.add(105);
+        list.add(106);
+        
+        
+        // Print the elements in the list
+        System.out.println(list);
+    }
+}
