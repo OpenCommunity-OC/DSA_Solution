2e44788ad9df1e0ea04ef653fe4ca3c0388d6b10
vicky acharjee <vickyacharjeevlg@gmail.com>
2024-03-10 23:36:39 +0530

    added for CeilingOfNum not optimized

--- FunctionsNmethods/intReturnType.java
+++ FunctionsNmethods/intReturnType.java
@@ -1,10 +1,13 @@
 package FunctionsNmethods;
 
 public class intReturnType {
+
     public static void main(String[] args) {
+
         //basic program to sum of two number
         int result= add(1,2);
         System.out.println(result);
+        
     }
 
     //passed value with return type of integer
